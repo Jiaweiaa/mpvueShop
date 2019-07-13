1f927e23bcbcf57c4437c2ae2eca0c2caf1a471b
dongpaopao <23521951@qq.com>
2019-07-13 22:15:44 +0800

    订单列表页点击穿透BUG修复

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  BASE_API: '"http://192.168.0.10:8003"'
+  // BASE_API: '"http://192.168.0.10:8003"'
   // BASE_API: '"http://192.168.0.8:8003"'
-  // BASE_API: '"https://mall.gaoshanapp.com"'
+  BASE_API: '"https://mall.gaoshanapp.com"'
   // BASE_API: '"http://120.27.1.42:8003"'
   // BASE_API: '"https://jf.ibaituan.cn"'
   // BASE_API: '"http://192.168.1.188:8003"'
