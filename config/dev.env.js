fc0cc63452ccb107770c9e7e8ee54349675cc1e2
dongpaopao <23521951@qq.com>
2019-07-24 16:25:42 +0800

    111

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,13 +4,13 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  BASE_API: '"http://192.168.0.10:8003"'
+  // BASE_API: '"http://192.168.0.10:8003"'
   // BASE_API: '"http://120.27.1.42:8003"'
   // BASE_API: '"http://192.168.0.8:8003"'
   // BASE_API: '"https://mall.gaoshanapp.com"'
   // BASE_API: '"http://192.168.1.109:8003"'
   // BASE_API: '"http://120.27.1.42:8003"'
-  // BASE_API: '"https://jf.ibaituan.cn"'
+  BASE_API: '"https://jf.ibaituan.cn"'
   // BASE_API: '"http://192.168.1.188:8003"'
   // BASE_API: '"http://192.168.3.29:8003"'
   // BASE_API: '"http://39.97.233.168:8003"'
