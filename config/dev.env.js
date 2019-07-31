5b8cf9031272f2715d8fd5a530312767574d34c2
dongpaopao <23521951@qq.com>
2019-07-31 14:36:52 +0800

    111

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
   // BASE_API: '"http://120.27.1.42:8003"'
-  BASE_API: '"https://jf.ibaituan.cn"'
-  // BASE_API: '"http://192.168.1.188:8003"'
+  // BASE_API: '"https://jf.ibaituan.cn"'
+  BASE_API: '"http://192.168.0.10:8003"'
 
 })
