46a640281980398f37b0e850d1a8e6af1807926c
dongpaopao <23521951@qq.com>
2019-08-05 11:48:08 +0800

    多店铺8.5日上午提交

---
+++ src/pages/storeIndex/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import App from "./index";
+
+const app = new Vue(App);
+app.$mount();
+export default {
+  config: {
+    backgroundTextStyle: "dark",
+    backgroundColor: "#fff",
+    navigationBarTitleText: "商品列表",
+    usingComponents: {
+      "van-popup": "/static/vant/popup/index",
+      "van-transition": "/static/vant/transition/index",
+      "van-cell": "/static/vant/cell/index",
+      "van-button": "/static/vant/button/index",
+      "van-icon": "/static/vant/icon/index",
+      "van-card": "/static/vant/card/index",
+      "van-tab": "/static/vant/tab/index",
+      "van-tabs": "/static/vant/tabs/index"
+    }
+  }
+};
