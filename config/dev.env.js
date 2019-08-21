f593c29b6fc3f8e0d2d5d69325babc51702868b5
dongpaopao <23521951@qq.com>
2019-08-21 09:17:29 +0800

    8.21日更新

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-21 08:45:10
+ * @LastEditTime: 2019-08-21 08:48:12
+ * @LastEditors: Please set LastEditors
+ */
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
@@ -5,7 +12,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
   // BASE_API: '"http://120.27.1.42:8003"'
-  //  BASE_API: '"https://mall.gaoshanapp.com"'
-  BASE_API: '"http://192.168.0.10:8003"'
+   BASE_API: '"https://mall.gaoshanapp.com"'
+  // BASE_API: '"http://192.168.0.10:8003"'
 
 })
