984acd1377f9e7383cb721a2ee3530ba5fc8f34b
dongpaopao <23521951@qq.com>
2019-08-23 15:29:55 +0800

    321

--- src/pages/goodsFlash/index.vue
+++ src/pages/goodsFlash/index.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-23 15:02:14
+ * @LastEditTime: 2019-08-23 15:02:14
+ * @LastEditors: your name
+ -->
 <template>
   <div class="goods">
     <!--
